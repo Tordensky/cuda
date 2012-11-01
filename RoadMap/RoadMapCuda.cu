7fccd9ce81b43a3317fcf058b69c6c36c2095a20
Simon Nistad <simon.nistad@hotmail.com>
2012-11-01 16:19:33 +0100

    First solution finished. Gets a segfault on high numbers

--- RoadMap/RoadMapCuda.cu
+++ RoadMap/RoadMapCuda.cu
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <cuda.h>
 
-#define WIDTH 500
-#define HEIGHT 500
+#define WIDTH 5000
+#define HEIGHT 5000
 #define SIZE WIDTH*HEIGHT
 
 int zooms=10;
@@ -29,20 +29,20 @@ int colortable[] = {
 
 int colorcount=10;
 
-double box_x_min, box_x_max, box_y_min, box_y_max;
+float box_x_min, box_x_max, box_y_min, box_y_max;
 
-inline double translate_x(int x) {       
+__device__ inline float translate_x(int x) {       
   return (((box_x_max-box_x_min)/WIDTH)*x)+box_x_min;
 }
 
-inline double translate_y(int y) {
+__device__ inline float translate_y(int y) {
   return (((box_y_max-box_y_min)/HEIGHT)*y)+box_y_min;
 }
 
-inline int solve(double x, double y)              //Simple Mandelbrot
+__device__ inline int solve(float x, float y)              //Simple Mandelbrot
 {                                          //divergation test
-  double r=0.0,s=0.0;
-  double next_r,next_s;
+  float r=0.0,s=0.0;
+  float next_r,next_s;
   int itt=0;
   
   while((r*r+s*s)<=4.0) {
@@ -55,32 +55,94 @@ inline int solve(double x, double y)              //Simple Mandelbrot
   return itt;
 }
 
-__global__ void cudaSolve(float result[WIDTH][HEIGHT])
+__global__ void cudaSolve(int *res, int width, int height, float x_min, float x_max, float y_min, float y_max)
 {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    
+    if (i <= (width*height)){
+      int x, y;
+    
+      // Calculate x and y from i
+      x = i % width;
+      y = i / width;
+                  
+      res[i] = solve((((x_max-x_min)/WIDTH)*x)+x_min, (((y_max-y_min)/HEIGHT)*y)+y_min)*10/100; 
+    }
 }
 
 
 void CreateMap() {        //Our 'main' function
+    
+  int x, y, size;                               //Holds the result of slove
   
-  int x,y,color;                               //Holds the result of slove
+  size = SIZE * sizeof(int);
+  // Allocate space for result on host
+  int *result;
+  result = (int*)malloc(sizeof(int)*SIZE);
   
+  // Allocate space for result on device
+  int *devResult;
+  cudaMalloc((void**)&devResult, size);
 
-  for(y=0;y<HEIGHT;y++)              //Main loop for map generation
-    for(x=0;x<WIDTH;x++){  
-      color = solve(translate_x(x), translate_y(y))*colorcount/100;
-      
+  // Calculate space and number of blocks
+  int threadsPerBlock = 512;
+  int blocksPerGrid = (SIZE + threadsPerBlock - 1) / threadsPerBlock;
+  
+  //printf("Thread Per block: %d, blocksPerGrid: %d\n", threadsPerBlock, blocksPerGrid);
+  
+  // Solve on Cuda device
+  cudaSolve<<<threadsPerBlock, blocksPerGrid>>>(devResult, WIDTH, HEIGHT, box_x_min, box_x_max, box_y_min, box_y_max);
+  
+  // Copy result back to host
+  cudaMemcpy(result, devResult, size, cudaMemcpyDeviceToHost);
+  
+  cudaFree(devResult);
+  
+  printf("Kommer hit før segfault\n");
+  
+  for (int i = 0; i < SIZE; i++){
+    x = i % WIDTH;
+    y = i / WIDTH;
+    
+    if (y > HEIGHT){
+      printf ("ERROR");
+    }
+    
 #ifdef GRAPHICS
-      gs_plot(x,y,colortable[color]); //Plot the coordinate to map
-
+    gs_plot(x,y,colortable[result[i]]); //Plot the coordinate to map
 #else
-      crc+=colortable[color];
+      crc+=colortable[result[i]];
 #endif
     }
 #ifdef GRAPHICS
   gs_update();
 #endif
+    
+  
+  
+  //result[0] = 123;
+//   printf("New round\n");
+//   for (int r = 0; r < SIZE; r++)
+//     if (result[r] > 10)
+//       printf("\tResult %d\n", result[r]);
+  
+//   for(y=0;y<HEIGHT;y++)              //Main loop for map generation
+//     for(x=0;x<WIDTH;x++){  
+//       color = solve(translate_x(x), translate_y(y))*colorcount/100;
+//       
+// #ifdef GRAPHICS
+//       gs_plot(x,y,colortable[color]); //Plot the coordinate to map
+// 
+// #else
+//       crc+=colortable[color];
+// #endif
+//     }
+// #ifdef GRAPHICS
+//   gs_update();
+// #endif
+  
+  
+  free(result); // TODO allocate and free before all and after all
 }
 
 
@@ -88,7 +150,7 @@ int
 RoadMap ()
 {
   int i;
-  double deltaxmin, deltaymin, deltaxmax,deltaymax;
+  float deltaxmin, deltaymin, deltaxmax,deltaymax;
 
   box_x_min=-1.5; box_x_max=0.5;           //Set the map bounding box for total map
     box_y_min=-1.0; box_y_max=1.0;
